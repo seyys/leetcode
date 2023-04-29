ef0c83ced7a0f576ae98ff88eb5cc0f86c11ebf6
Daniel Yang <danielysyang@gmail.com>
2023-04-29 23:07:53 +1200

    Solved 141: 23/23 cases passed (81 ms) Your runtime beats 6.03 % of python3 submissions Your memory usage beats 9.33 % of python3 submissions (20.3 MB)

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
+# @lc code=end
