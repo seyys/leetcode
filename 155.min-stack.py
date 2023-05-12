55bd4f587cb6210cb3d39e7600387a283c32ac53
Daniel Yang <danielysyang@gmail.com>
2023-05-13 01:26:09 +1200

    Solved 155: 31/31 cases passed (66 ms) Your runtime beats 49.97 % of python3 submissions Your memory usage beats 21.57 % of python3 submissions (20.2 MB)

---
+++ 155.min-stack.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=155 lang=python3
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min) == 0 or val <= self.min[-1]:
+            self.min.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min[-1]:
+            self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
