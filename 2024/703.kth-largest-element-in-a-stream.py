1b8eaa13758253bb23cd60a0fac899a61bd17c40
Daniel Yang <danielysyang@gmail.com>
2025-04-06 01:54:07 +1100

    703. 12/12 cases passed (19 ms) Your runtime beats 48.8 % of python3 submissions Your memory usage beats 45.76 % of python3 submissions (23.7 MB)

---
+++ 2024/703.kth-largest-element-in-a-stream.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=703 lang=python3
+#
+# [703] Kth Largest Element in a Stream
+#
+
+# @lc code=start
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+# @lc code=end
+
