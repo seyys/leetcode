9453d0df864c1b5a16a58a7442d541ccb3635a8f
Daniel Yang <danielysyang@gmail.com>
2025-04-07 00:39:33 +1000

    70. 45/45 cases passed (0 ms) Your runtime beats 100 % of python3 submissions Your memory usage beats 8.53 % of python3 submissions (18 MB)

---
+++ 2024/70.climbing-stairs.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    @functools.cache
+    def climbStairs(self, n: int) -> int:
+        if n < 1:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+# @lc code=end
+
